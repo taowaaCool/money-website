5ff4038224e07bf3d62445585b178806359aea1f
torreyxian <xianxt0570@163.com>
2021-02-22 20:26:10 +0800

    deploy

---
+++ precache-manifest.abd39725d70f8211deb6ed926c1ec507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4da8086742ccd052a58",
+    "url": "/money-website/css/app.a9fdb4da.css"
+  },
+  {
+    "revision": "65d7aa3501365a9cb7f9764d6c85c1b9",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "f4da8086742ccd052a58",
+    "url": "/money-website/js/app.c8f3a4d3.js"
+  },
+  {
+    "revision": "2ce6a3a33d4d1993d984",
+    "url": "/money-website/js/chunk-vendors.6d0061ac.js"
+  },
+  {
+    "revision": "5a3bc337ed9627c080692ef3c952b5ba",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
